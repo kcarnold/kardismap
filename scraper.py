d80bb1d5d7c998651602db9dd91ef9a7b39b2295
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2013-02-23 03:31:01 -0500

    Fix stupid mistake

--- scraper.py
+++ scraper.py
@@ -2,14 +2,12 @@ from lxml.html import fromstring
 import requests
 from ghost import Ghost
 ghost = Ghost(download_images=False)
-print 'initial'
-page, resources = ghost.open('http://www.guidestar.org/AdvancedSearch.aspx')
 
 selectors = dict(name='h1.org-name', missionStatement='#mission .html-snippet')
 
 
 def getDetailUrlForEIN(ein):
-    print 'search'
+    page, resources = ghost.open('http://www.guidestar.org/AdvancedSearch.aspx')
     result, resources = ghost.evaluate('document.querySelector("#ctl00_phMainBody_orgSearchConfiguration_keywords_txtValue").value = "{}"'.format(ein))
     ghost.click('''form#aspnetForm div#ctl00_divPageContainer.page-container div.page-content div#ctl00_phMainBody_advSearches.adv-searches div#ctl00_phMainBody_2.tab-contents fieldset.one-col input.form-button''', expect_loading=True)
     return ghost.evaluate('''document.querySelector('#ctl00_phMainBody_rptSearchResults_ctl00_aOrganizationName').getAttribute('href')''')[0]
